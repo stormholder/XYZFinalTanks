80ed5b61b6c4814768c2d57be9074ad93a2cf228
Alexander <dantedmc78@gmail.com>
2024-08-19 22:07:53 +0300

    update bulletfactory

--- Tanks/BulletFactory.cs
+++ Tanks/BulletFactory.cs
@@ -3,9 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using XYZFinalTanks.Tanks.Entity;
 
 namespace XYZFinalTanks.Tanks;
 
 internal class BulletFactory
 {
+    private static BulletFactory? instance;
+    private BulletFactory() { }
+
+    public static BulletFactory GetInstance()
+    {
+        instance ??= new BulletFactory();
+        return instance;
+    }
+
+    public Bullet CreateBullet(Cell position, Direction direction)
+    {
+        return new Bullet(position, direction);
+    }
 }
