6440b429c9e113449dcadc8bfc6c04a7711d0613
Alexander <dantedmc78@gmail.com>
2024-08-20 22:10:25 +0300

    update main

--- Program.cs
+++ Program.cs
@@ -17,25 +17,17 @@ internal class Program
         var gameLogic = new TankGameLogic(gameData);
         gameLogic.InitializeInput(input);
 
-        //var map = new Map(gameData.LevelMaps[1]);
-        //var bullet = new Bullet(new(40, 10), Direction.Left);
-
         var lastFrameTime = DateTime.Now;
         while (true)
         {
             var frameStartTime = DateTime.Now;
             float deltaTime = (float)(frameStartTime - lastFrameTime).TotalSeconds;
-            //bullet.Update(deltaTime);
             input.Update();
             gameLogic.DrawNewState(deltaTime, currRenderer);
-            //map.Render(currRenderer);
-            //bullet.Render(currRenderer);
             lastFrameTime = frameStartTime;
-            if (!currRenderer.Equals(prevRenderer)) currRenderer.Render();
+            if (!currRenderer.Equals(prevRenderer)) currRenderer.Render(prevRenderer);
 
-            var tmp = prevRenderer;
-            prevRenderer = currRenderer;
-            currRenderer = tmp;
+            (currRenderer, prevRenderer) = (prevRenderer, currRenderer);
             currRenderer.Clear();
 
             var nextFrameTime = frameStartTime + TimeSpan.FromSeconds(targetFrameTime);
