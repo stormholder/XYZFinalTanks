9807d3bf275728191d797b461dbce5e747e3bc83
Alexander <dantedmc78@gmail.com>
2024-08-21 06:55:58 +0300

    update entitybase

--- Tanks/Entity/EntityBase.cs
+++ Tanks/Entity/EntityBase.cs
@@ -4,12 +4,14 @@ namespace XYZFinalTanks.Tanks.Entity;
 
 internal abstract class EntityBase : IUpdateable, IRenderable, IDisposable
 {
+    public bool IsDisposed { get; protected set; } = false;
     public int Health { get; set; }
     public Cell Position { get; set; }
+    public int Speed { get; set; }
 
     public virtual void Dispose()
     {
-        //
+        IsDisposed = true;
     }
 
     public abstract void Render(IRenderer renderer);
