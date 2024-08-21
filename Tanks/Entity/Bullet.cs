be9d3c01e9534e7d1a15ccd7704acb05e9659a9c
Alexander <dantedmc78@gmail.com>
2024-08-21 06:56:27 +0300

    refactor: Bullet - once again, it IS an Entity after all

--- Tanks/Entity/Bullet.cs
+++ Tanks/Entity/Bullet.cs
@@ -2,30 +2,33 @@
 
 namespace XYZFinalTanks.Tanks.Entity;
 
-internal class Bullet : IUpdateable, IRenderable, IDisposable
+internal class Bullet : EntityBase
 {
     private float _timeToMove = 0f;
     private Direction _direction;
-    public event Action? OnCollision;
     const int Tick = 2;
-    const char bullet = 'º';
-    private Cell _position;
-    public bool IsDisposed = false;
+    private readonly char[,] bullet = new[,]
+    {
+        { ' ', 'º', ' ', ' ' },
+        { ' ', ' ', ' ', ' ' }
+    };
 
     public Bullet(Cell position, Direction direction)
     {
-        _position = position;
+        Position = position;
         _direction = direction;
     }
 
-    public Direction Direction => _direction;
-    public Cell Position => _position;
-
-    public void Render(IRenderer renderer)
+    public override void Render(IRenderer renderer)
     {
-        if (_position.X < 0 || _position.Y < 0 || _position.X >= renderer.GetWidth() || _position.Y >= renderer.GetHeight())
-            return;
-        renderer.SetPixel(_position.X, _position.Y, bullet, 2);
+        if (!IsDisposed)
+            for (int i = 0; i < Position.Height; i++)
+            {
+                for (int j = 0; j < Position.Width; j++)
+                {
+                    renderer.SetPixel(Position.X * Position.Width + j, Position.Y * Position.Height + i, bullet[i, j], 2);
+                }
+            }
     }
 
     public void Shift(Direction dir)
@@ -33,55 +36,28 @@ internal class Bullet : IUpdateable, IRenderable, IDisposable
         switch (dir)
         {
             case Direction.Up:
-                _position.Y--;
+                Position = new Cell(Position.X, Position.Y - 1);
                 break;
             case Direction.Down:
-                _position.Y++;
+                Position = new Cell(Position.X, Position.Y + 1);
                 break;
             case Direction.Left:
-                _position.X--;
+                Position = new Cell(Position.X - 1, Position.Y);
                 break;
             case Direction.Right:
-                _position.X++;
+                Position = new Cell(Position.X + 1, Position.Y);
                 break;
             default:
                 break;
         };
     }
 
-    public void Update(float deltaTime)
+    public override void Update(float deltaTime)
     {
         _timeToMove -= deltaTime;
         if (_timeToMove > 0f)
             return;
-        _timeToMove = 1 / (Tick);
+        _timeToMove = 1 / Tick;
         Shift(_direction);
-        //if (_position.X < 0 || _position.Y < 0)
-        //{
-        //    Dispose();
-        //}
-        //var _lvl = LevelModel.GetInstance();
-        //if (_position.X >= _lvl.Map.Width || _position.Y >= _lvl.Map.Height)
-        //{
-        //    Dispose();
-        //}
-        //// TODO check collisions with EVERYTHING
-        //foreach (var wall in _lvl.Map.Walls)
-        //{
-        //    if (wall.Position.X == _position.X && wall.Position.Y == _position.Y)
-        //    {
-        //        //result = false;
-        //        // TODO reduce wall health
-        //        wall.Health--;
-        //        Dispose();
-        //        break;
-        //    }
-        //}
-    }
-
-    public void Dispose()
-    {
-        // TODO
-        IsDisposed = true;
     }
 }
